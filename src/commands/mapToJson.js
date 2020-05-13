cd4d8fdbb552cfa1cc17d54bb9b85009a32f456b
Samuel Pordeus <samuelspordeus@gmail.com>
2020-05-13 10:16:44 -0300

    1.0.1

--- src/commands/mapToJson.js
+++ src/commands/mapToJson.js
@@ -8,12 +8,12 @@ function handler() {
   var json = highlight.replace(/%{/g, '{')
 
   // This logic is terrible and needs to be improved,
-  // specially the hash map RegEx.
+  // specially the atom map RegEx.
   if (highlight.match(/"([a-zA-Z0-9-]+)" =>/g)) {
     // It's a string map!
     json = json.replace(/"([a-zA-Z0-9-]+)" => /g, '\"$1\": ')
   } else {
-    // It's a hash map! (please send help)
+    // It's an atom map! (please send help)
     json = json.replace(/([a-zA-Z0-9-]+): /g, '\"$1\": ')
   }
 
